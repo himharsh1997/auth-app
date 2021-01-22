0c883eae1cc9afebf640975b990a2e3858f5bf27
Himanshu Joshi <himanshu.joshi@digitaltrons.com>
2021-01-22 13:49:28 +0530

    change scope

--- src/fb.config.js
+++ src/fb.config.js
@@ -3,7 +3,7 @@ const queryString = require('query-string');
   const stringifiedParams = queryString.stringify({
     client_id: '235821818120555' || process.env.FACEBOOK_APIID,
     redirect_uri: 'https://auth-app-test-01.herokuapp.com/',
-    scope: ['email', 'user_friends'].join(','), // comma seperated string
+    scope: ['email'].join(','), // comma seperated string
     response_type: 'code',
     auth_type: 'rerequest',
     display: 'popup',
